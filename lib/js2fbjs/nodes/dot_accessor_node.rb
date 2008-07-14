26e4ecb4fffcb6f4e7c8b8bef4b1904f271f5f3d
Richard Jordan <jordanr@cs.washington.edu>
2008-07-14 21:02:55 +0000

    redoing with s-expressions, ParseTree a dependency right now

--- lib/js2fbjs/nodes/dot_accessor_node.rb
+++
@@ -1,9 +0,0 @@
-module RKelly
-  module Nodes
-    class DotAccessorNode < Node
-      def == (other)
-	(value==other.value) && (accessor == other.accessor)
-      end
-    end
-  end
-end
--- lib/js2fbjs/nodes/function_call_node.rb
+++
@@ -1,6 +0,0 @@
-module RKelly
-  module Nodes
-    class FunctionCallNode
-    end
-  end
-end
